5fcddbadaa05880383e1e1e24061daabf00ec6d0
Mariana Frydrak <maryanafrydrak@gmail.com>
2026-09-16 23:14:12 +0300

    add hero section and initial header structure

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -9,12 +9,22 @@ export default function HomePage() {
           <h1>Make Life Easier for the Family:</h1>
           <p>Find Babysitters Online for All Occasions</p>
           <Link to="/nannies" className="hero-button">
-            Get started ↗
+            Get started
+            <svg className="icon-up-right" width="14" height="16">
+              <use href="/icons.svg#icon-arrow-up-right" />
+            </svg>
+            <svg className="icon-right" width="16" height="16">
+              <use href="/icons.svg#icon-arrow-right" />
+            </svg>
           </Link>
         </div>
         <div className="hero-image-container">
           <div className="hero-badge">
-            <div className="badge-icon">✓</div>
+            <div className="badge-icon">
+              <svg width="30" height="30">
+                <use href="/icons.svg#icon-check" />
+              </svg>
+            </div>
             <div className="badge-content">
               <span className="badge-title">Experienced nannies</span>
               <span className="badge-count">15,000</span>
