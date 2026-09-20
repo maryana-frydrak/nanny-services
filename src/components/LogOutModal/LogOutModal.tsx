73789f50fa8e704df85f6155e43e90908086617b
Mariana Frydrak <maryanafrydrak@gmail.com>
2026-09-20 14:14:39 +0300

    add hover, focus to btns

--- src/components/LogOutModal/LogOutModal.tsx
+++ src/components/LogOutModal/LogOutModal.tsx
@@ -39,16 +39,12 @@ const LogoutModal: React.FC<LogoutModalProps> = ({
         <div className="logout-buttons-wrapper">
           <button
             type="button"
-            className="logout-btn confirm-btn"
+            className="logout-confirm-btn"
             onClick={onConfirm}
           >
             Log out
           </button>
-          <button
-            type="button"
-            className="logout-btn cancel-btn"
-            onClick={onClose}
-          >
+          <button type="button" className="logout-cancel-btn" onClick={onClose}>
             Cancel
           </button>
         </div>
